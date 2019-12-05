14038f2a483f4a483cac7de8bf6522ba65341c78
iandstevens92 <iandstevens92@gmail.com>
2019-12-05 18:49:08 +0200

    removing api keys

--- src/config/aws.js
+++ src/config/aws.js
@@ -3,9 +3,9 @@ import aws from "aws-sdk";
 aws.config.update({
 	apiVersion: "2015-03-31",
 	endpoint: "http://localhost:3000",
-	accessKeyId: "AKIATX4D5TFUTXMI4WOF",
-	secretAccessKey: "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",
-	region: "eu-west-1"
+	accessKeyId: "",
+	secretAccessKey: "",
+	region: "eu-west-1",
 });
 
 const lambda = new aws.Lambda({ region: "eu-west-1" });
